09d91305a0b4f0386d80b00feb9256b08301041b
MPacris <mpacris@yahoo.com>
2023-07-05 11:24:21 -0500

    udpated index file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,4 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-
-
+ReactDOM.render(<App />, document.getElementById('root'));
